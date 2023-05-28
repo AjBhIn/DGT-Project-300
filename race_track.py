80d1dccb6d9a437570964905236059dda38de372
AjBhIn <oldajaybhati1234@gmail.com>
2023-05-28 15:53:47 +1200

    28/05/23 Breifly Finished the White Board

--- race_track.py
+++ race_track.py
@@ -3,29 +3,31 @@ import pygame as pg
 
 
 # constant variables such colour, width. height
-line_colour = (26, 83, 92)
-line_width = 10
-line_height = 2
-line_pos_x = 0
+TRACK_COLOUR = (26, 83, 92)
+LINE_WIDTH = 10
+LINE_HEIGHT = 2
+LINE_POS_X = 0
+TRACK_LANE_OPACITY = 179
 
 # Creates lines using surfaces
 class TrackLineCreator:
 
-    def __init__(self, pos_y, width = line_width , height = line_height, pos_x  = line_pos_x, rgba = line_colour):
+    def __init__(self, pos_y, width = LINE_WIDTH , height = LINE_HEIGHT, pos_x  = LINE_POS_X, rgba = TRACK_COLOUR, opacity = TRACK_LANE_OPACITY):
 
         # setup of line / general info about line: line colour, width, opacity, and more
         self.pos = pos_x, pos_y
         self.geometry = width, height 
         self.window_width = 1042
         self.line_space_width = 12
-        self.opacity = 179
+        self.opacity = opacity
         self.line_surface = pg.Surface(self.geometry)
         self.line_surface.fill(rgba) 
         self.line_surface.set_alpha(self.opacity) 
 
         # creating more than one line at a time
         self.lines = []
-        for number_of_lines in range(int(self.window_width//self.line_space_width) + 1): # this tells the computer how many lines are needed in respect to window's width and space in between lines
+        # this tells the computer how many lines are needed in respect to window's width and space in between lines
+        for number_of_lines in range(int(self.window_width//self.line_space_width) + 1):
             self.lines.append([self.line_surface, (number_of_lines*self.line_space_width, self.pos[1])])
 
 
@@ -35,8 +37,10 @@ second_lane = TrackLineCreator(116)
 thrid_lane = TrackLineCreator(174)
 forth_lane = TrackLineCreator(232)
 fifth_lane = TrackLineCreator(290)
+
 starting_lane = TrackLineCreator(0, 1, 347, 59)
 finishing_lane = TrackLineCreator(0, 1, 347, 982)
+divider_lane = TrackLineCreator(388, 1, 310, 521, TRACK_COLOUR, 255) # Divider lane in the board
 
 # Has all the trakcs in a list as it is easy to import while using less variables
 tracks = [first_lane.lines, second_lane.lines, thrid_lane.lines, forth_lane.lines, fifth_lane.lines]
